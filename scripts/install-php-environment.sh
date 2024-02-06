ad6020d77d5770c4c7cd0feb9f100cbf1e5841ed
Josh Rainwater <jrainwat@gmail.com>
2024-02-06 10:55:24 -0800

    Cleanup install scripts. Currently set to 8.2

--- scripts/install-php-environment.sh
+++ scripts/install-php-environment.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
-apt-get install php8.3-cli php8.3-curl php8.3-opcache php8.3-xml php8.3-common php8.3-gd php8.3-readline php8.3-zip php8.3-fpm php8.3-mbstring
+sudo apt-get install php8.2 php8.2-cli php8.2-curl php8.2-opcache php8.2-xml php8.2-common php8.2-gd php8.2-readline php8.2-zip php8.2-fpm php8.2-mbstring -y
 
 ./install-composer.sh
 
 # PHPActor install
 curl -Lo phpactor.phar https://github.com/phpactor/phpactor/releases/latest/download/phpactor.phar
-chmod +x phpactor.phar
+sudo chmod +x phpactor.phar
 mv phpactor.phar ~/.local/bin/phpactor
 
 # php-cs-fixer install
-curl -L https://cs.symfony.com/download/php-cs-fixer-v3.phar -o php-cs-fixer
-chmod a+x php-cs-fixer
-mv php-cs-fixer ~/.local/bin
+curl -Lo php-cs-fixer.phar https://cs.symfony.com/download/php-cs-fixer-v3.phar
+sudo chmod a+x php-cs-fixer.phar
+mv php-cs-fixer.phar ~/.local/bin/php-cs-fixer
 
 # Laravel Installer
 composer global require laravel/installer:^5.5
--- scripts/ubuntu-setup.sh
+++ scripts/ubuntu-setup.sh
@@ -1,29 +1,32 @@
 #!/bin/sh
 
 # Upgrade and add the Repos we need
-apt upgrade && apt update && apt install ca-certificates apt-transport-https software-properties-common lsb-release -y
+sudo apt upgrade -y && sudo apt update -y && sudo apt install ca-certificates apt-transport-https software-properties-common lsb-release -y
 
 # Github API
-type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
 && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
 && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
 
-# For PHP
-add-apt-repository ppa:ondrej/php -y
-# For Neovim
-add-apt-repository ppa:neovim-ppa/unstable
+# Add extra repositories
+sudo add-apt-repository ppa:ondrej/php -y
+sudo add-apt-repository ppa:neovim-ppa/unstable -y
 
 #---
-apt update && apt upgrade
+sudo apt update -y && sudo apt upgrade -y
 
-# Basic Software
-apt-get install openssl git fzf gcc curl gnupg gh neovim -y
+# The basics
+sudo apt-get install openssl git fzf gcc g++ curl gnupg gh neovim zip unzip -y
 
 # Aws CLI
+# not working?
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
+rm -rf aws awscliv2.zip
+
+# Because this doesn't exist by default
+mkdir ~/.local/bin
 
 # Run the other scripts.
 ./install-lazygit.sh

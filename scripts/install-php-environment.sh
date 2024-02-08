fa2d8751469bb6e7a308e06a0e661b52e2768010
Josh Rainwater <jrainwat@gmail.com>
2024-02-08 12:48:05 -0800

    Back to php8.3

--- scripts/install-php-environment.sh
+++ scripts/install-php-environment.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt-get install php8.2 php8.2-cli php8.2-curl php8.2-opcache php8.2-xml php8.2-common php8.2-gd php8.2-readline php8.2-zip php8.2-fpm php8.2-mbstring php8.2-sqlite3 -y
+sudo apt-get install php8.3 php8.3-cli php8.3-curl php8.3-opcache php8.3-xml php8.3-common php8.3-gd php8.3-readline php8.3-zip php8.3-fpm php8.3-mbstring php8.3-sqlite3 -y
 
 # Install Box to build Phars
 curl -LSs https://box-project.github.io/box2/installer.php | php

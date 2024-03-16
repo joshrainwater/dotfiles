d96f46a662217fecd8039bcca8412f86774d5c1b
Josh Rainwater <jrainwat@gmail.com>
2024-03-16 10:29:26 -0700

    Go test aliases, disable codeium by default

--- nvim/lua/rainwater/lazy/codeium.lua
+++ nvim/lua/rainwater/lazy/codeium.lua
@@ -2,12 +2,13 @@ return {
     "Exafunction/codeium.vim",
     event = "BufEnter",
     config = function ()
-          vim.keymap.set('i', '<C-y>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
-          vim.keymap.set('i', '<C-j>', function () return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
-          vim.keymap.set('i', '<C-k>', function () return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
-          vim.keymap.set('i', '<C-x>', function () return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
+        vim.keymap.set('i', '<C-y>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+        vim.keymap.set('i', '<C-j>', function () return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
+        vim.keymap.set('i', '<C-k>', function () return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
+        vim.keymap.set('i', '<C-x>', function () return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
 
-          vim.g.codeium_disable_bindings = true
+        vim.g.codeium_disable_bindings = true
+        vim.g.enable_codeium = false 
 
     end,
     opts = {

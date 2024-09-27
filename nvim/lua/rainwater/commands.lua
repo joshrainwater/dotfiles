52094a98a67987499c5220caf5ad5a188a17c415
Josh Rainwater <jrainwater@intellaegis.com>
2024-09-27 07:26:01 -0700

    Add fix command -- the work terminal won't retain tab size

--- nvim/lua/rainwater/commands.lua
+++ nvim/lua/rainwater/commands.lua
@@ -1,2 +1,4 @@
 -- Turn on "reader" mode
 vim.api.nvim_create_user_command('Read', ':set wrap linebreak nolist', {})
+vim.api.nvim_create_user_command('Fix', ':set shiftwidth=4 | set tabstop=4', {})
+

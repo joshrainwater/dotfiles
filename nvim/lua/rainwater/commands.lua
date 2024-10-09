e6c87e077adec0335bad3573b71aa222702a312a
Josh Rainwater <jrainwater@intellaegis.com>
2024-10-09 10:19:52 -0700

    Rename the command for me

--- nvim/lua/rainwater/commands.lua
+++ nvim/lua/rainwater/commands.lua
@@ -1,4 +1,4 @@
 -- Turn on "reader" mode
-vim.api.nvim_create_user_command('Read', ':set wrap linebreak nolist', {})
+vim.api.nvim_create_user_command('Write', ':set wrap linebreak nolist', {})
 vim.api.nvim_create_user_command('Fix', ':set shiftwidth=4 | set tabstop=4', {})
 

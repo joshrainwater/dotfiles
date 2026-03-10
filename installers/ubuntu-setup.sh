acbda1f8dfb013477915a4bb993d599dcd6e2e14
Josh Rainwater <jrainwater@intellaegis.com>
2026-03-10 16:05:21 -0700

    clean up a little; install tools

--- installers/ubuntu-setup.sh
+++ installers/ubuntu-setup.sh
@@ -8,15 +8,14 @@ curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo 
 && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
 && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
 
-# Add extra repositories
+# PHP and Neovim repositories.
 sudo add-apt-repository ppa:ondrej/php -y
 sudo add-apt-repository ppa:neovim-ppa/unstable -y
 
-#---
 sudo apt update -y && sudo apt upgrade -y
-
-# The basics
-sudo apt-get install openssl git fzf gcc g++ curl gnupg gh neovim zip unzip sqlite3 zoxide neofetch powerline ripgrep fd-find -y
+sudo apt-get install openssl git gcc g++ curl gnupg zip unzip sqlite3 -y #Basics
+sudo apt-get install gh neovim -y #Apps
+sudo apt-get install fzf zoxide neofetch ripgrep fd-find eza bat btop git-delta -y #Better Utils
 
 # Aws CLI
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
